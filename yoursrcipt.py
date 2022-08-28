97be580faa82166354b67c38319772867c270e13
ri27luq <kaichun.lin@campus.lmu.de>
2022-08-28 12:44:20 +0200

    Minor change

--- yoursrcipt.py
+++ yoursrcipt.py
@@ -4,6 +4,10 @@ import dimod
 @click.command()
 
 def cli():
+    ### Matrix
+    #    0  1
+    # 0 -1  2
+    # 1  -  1
     bqm = dimod.BinaryQuadraticModel({0: -1, 1: 1}, {(0, 1): 2}, 0.0, dimod.BINARY)
     sampleset = dimod.ExactSolver().sample(bqm)
     print(sampleset)
